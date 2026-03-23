b3ceed630c2b41b415445fcf5da68a4efbd8c085
Huzaifa Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2026-03-23 17:25:19 +0500

    bjhvcv
    hbvhc

--- clacu.js
+++ clacu.js
@@ -13,7 +13,7 @@ let { num1, num2, operation } = await inquirer.prompt([{
         name: "operation",
         type: "list",
         choices: ["add", "sub", "mul", "div"],
-        message: "Enter first number1:",
+        message: "Enter first number:",
     },
 ]);
 //console.log(answers);
